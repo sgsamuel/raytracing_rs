401203341802c127462f08451db84f732f186ac0
Shamay Samuel <shamay.samuel@gmail.com>
2024-12-28 19:03:52 -0500

    Logging

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,25 @@
+use dotenv::dotenv;
+use log::info;
+use env_logger;
+
+#[derive(Debug)]
+pub struct Config {
+    pub rust_log: String,
+}
+
+impl Config {
+    pub fn init() -> Config {
+        Config {
+            rust_log: std::env::var("RUST_LOG")
+                .expect("RUST_LOG must be specified"),
+        }
+    }
+}
+
 fn main() {
+  dotenv().ok();
+  env_logger::init();
+
   let image_width: u32 = 256;
   let image_height: u32 = 256;
   
@@ -8,17 +29,16 @@ fn main() {
 
   for j in 0..image_height {
     for i in 0..image_width {
-        // Calculate RGB values
+        info!("Scanlines remaining: {}", image_height - j);
+        
         let r = i as f64 / (image_width - 1) as f64;
         let g = j as f64 / (image_height - 1) as f64;
         let b = 0.0;
 
-        // Convert to integer values in the range [0, 255]
         let ir = (255.999 * r) as i32;
         let ig = (255.999 * g) as i32;
         let ib = (255.999 * b) as i32;
 
-        // Output the RGB values
         println!("{} {} {}", ir, ig, ib);
     }
 }
