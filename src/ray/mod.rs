73029fb61df4129916cf2a73f232b584d9bc9efa
Shamay Samuel <shamay.samuel@gmail.com>
2024-12-30 09:16:57 -0500

    Ray tests

--- src/ray/mod.rs
+++ src/ray/mod.rs
@@ -28,3 +28,32 @@ impl Ray {
         self.orig + self.dir * t
     }
 }
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn origin_and_direction() {
+        let orig: Point3 = Point3::new(3.0, 2.0, 1.0);
+        let dir: Vec3 = Vec3::new(1.0, 2.0, 3.0);
+        let ray: Ray = Ray::new(orig, dir);
+        assert_eq!(ray.origin(), &orig);
+        assert_eq!(ray.direction(), &dir);
+    }
+
+    #[test]
+    fn at() {
+        let orig: Point3 = Point3::new(3.0, 2.0, 1.0);
+        let dir: Vec3 = Vec3::new(1.0, 2.0, 3.0);
+        let ray: Ray = Ray::new(orig, dir);
+
+        assert_eq!(ray.at(0.0), Point3::new(3.0, 2.0, 1.0));
+        assert_eq!(ray.at(0.5), Point3::new(3.5, 3.0, 2.5));
+        assert_eq!(ray.at(1.0), Point3::new(4.0, 4.0, 4.0));
+
+        
+        assert_eq!(ray.at(2.0), Point3::new(5.0, 6.0, 7.0));
+    }
+}
